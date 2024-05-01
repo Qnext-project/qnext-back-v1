f874ba39ae408afb8cfed888850838082716c583
bahmankhah <47614811+bahmankhah@users.noreply.github.com>
2024-05-01 22:40:59 +0330

    fix empty room

--- app/Http/Controllers/DoctorController.php
+++ app/Http/Controllers/DoctorController.php
@@ -163,7 +163,7 @@ return response()->noContent();
         $audios = [];
         $audios['num'] = Media::where('name', 'LIKE', '%Shomare%')->first();
         $audios['room'] = Media::where('name', 'LIKE', '%به اتاقِ%')->first();
-        $audios['room_num'] = Media::find($room?->media_id) ?? Media::where('name', 'LIKE', "%{$userUp->doc_info['room']}%")->first();
+ //       $audios['room_num'] = Media::find($room?->media_id) ?? Media::where('name', 'LIKE', "%{$userUp->doc_info['room']}%")->first();
 /*         $audios['title'] = Media::find($expTitle?->media_id);
         $audios['expertise'] = Media::find($exp?->media_id); */
         $num = intval($userUp->current_turn_number);
